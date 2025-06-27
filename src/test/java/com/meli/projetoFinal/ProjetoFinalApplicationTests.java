271fb95a07ddcebd87ca3a17ec4674b80a381495
Igor Ujiie <igorkazuhico@hotmail.com>
2025-06-27 15:23:30 -0300

    subindo projeto vazio

---
+++ src/test/java/com/meli/projetoFinal/ProjetoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.projetoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
