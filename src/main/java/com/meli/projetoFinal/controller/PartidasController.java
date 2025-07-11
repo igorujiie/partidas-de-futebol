02bedf88ff01da4a3198437de8ac69b58c61854f
Igor Ujiie <igorkazuhico@hotmail.com>
2025-07-11 14:26:53 -0300

    subindo metodos de partidas e swagger

--- src/main/java/com/meli/projetoFinal/controller/PartidasController.java
+++ src/main/java/com/meli/projetoFinal/controller/PartidasController.java
@@ -5,10 +5,13 @@ import com.meli.projetoFinal.model.Partidas;
 import com.meli.projetoFinal.service.PartidasService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/partidas")
@@ -24,9 +27,27 @@ public class PartidasController {
         return ResponseEntity.status(201).body(partida);
     }
 
-//    @GetMapping
-//    public ResponseEntity<List<PartidasDTO>> listarPartidas() {
-//        List<PartidasDTO> partidas = partidasService.listarPartidas();
-//        return ResponseEntity.ok(partidas);
-//    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Partidas> atualizarPartida(@PathVariable Long id, @RequestBody @Valid PartidasDTO partidasDTO) {
+        Partidas partida = partidasService.atualizarPartida(id,partidasDTO);
+        return ResponseEntity.status(200).body(partida);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Partidas>> listarPartidas(Pageable pageable) {
+        Page<Partidas> partidas = partidasService.getPartidas(pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(partidas);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Partidas> buscarPartida(@PathVariable Long id){
+        Partidas partida = partidasService.getPartida(id);
+        return ResponseEntity.status(HttpStatus.OK).body(partida);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Partidas> deletarPartida(@PathVariable Long id) {
+        partidasService.deletePartida(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
--- src/main/java/com/meli/projetoFinal/service/PartidasService.java
+++ src/main/java/com/meli/projetoFinal/service/PartidasService.java
@@ -12,11 +12,14 @@ import com.meli.projetoFinal.repository.EstadioRepository;
 import com.meli.projetoFinal.repository.PartidasRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class PartidasService {
@@ -75,15 +78,62 @@ public class PartidasService {
         return partidasRepository.save(partida);
     }
 
-    private PartidasDTO toDTO(Partidas partida) {
-        PartidasDTO dto = new PartidasDTO();
-        dto.setClubeCasa(partida.getClubeCasa().getId());
-        dto.setClubeVisitante(partida.getClubeVisitante().getId());
-        dto.setEstadioId(partida.getEstadio().getId());
-        dto.setGolsCasa(partida.getGolsCasa());
-        dto.setGolsVisitante(partida.getGolsVisitante());
-        dto.setDataPartida(partida.getDataPartida());
-        return dto;
+    @Transactional
+    public Partidas atualizarPartida(Long id, PartidasDTO dto) {
+        Partidas partida = partidasRepository.findById(id)
+                .orElseThrow(() -> new DadoNaoEncontradoException("Partida não encontrada."));
+
+        Clube clubeCasa = clubeRepository.findById(dto.getClubeCasa())
+                .orElseThrow(() -> new DadoNaoEncontradoException("Clube casa não encontrado."));
+        Clube clubeVisitante = clubeRepository.findById(dto.getClubeVisitante())
+                .orElseThrow(() -> new DadoNaoEncontradoException("Clube visitante não encontrado."));
+
+        if (!clubeCasa.getAtivo() || !clubeVisitante.getAtivo()) {
+            throw new ConflitoDeDadosException("Um dos clubes está inativo.");
+        }
+        if (dto.getDataPartida().isBefore(clubeCasa.getDataCriacao()) ||
+                dto.getDataPartida().isBefore(clubeVisitante.getDataCriacao())) {
+            throw new ConflitoDeDadosException("Data da partida não pode ser anterior à data de criação de um dos clubes.");
+        }
+
+        Estadio estadio = estadioRepository.findById(dto.getEstadioId())
+                .orElseThrow(() -> new DadosInvalidosException("Estádio não encontrado."));
+
+        LocalDate dataPartida = dto.getDataPartida();
+        LocalDate min = dataPartida.minusDays(2);
+        LocalDate max = dataPartida.plusDays(2);
+
+        boolean clubeCasaConflito = partidasRepository.existsByClubeCasaAndDataPartidaBetween(clubeCasa, min, max);
+        boolean clubeVisitanteConflito = partidasRepository.existsByClubeVisitanteAndDataPartidaBetween(clubeVisitante, min, max);
+
+        if (clubeCasaConflito || clubeVisitanteConflito) {
+            throw new ConflitoDeDadosException("Um dos clubes já possui outra partida marcada em menos de 48 horas.");
+        }
+
+        boolean estadioOcupado = partidasRepository.existsByEstadioAndDataPartida(estadio, dataPartida);
+        if (estadioOcupado) {
+            throw new ConflitoDeDadosException("O estádio já possui jogo marcado nesse dia.");
+        }
+
+        partida.setClubeCasa(clubeCasa);
+
+        return partidasRepository.save(partida);
+    }
+
+
+    public Partidas getPartida(Long id) {
+        return partidasRepository.findById(id).orElseThrow(() -> new DadoNaoEncontradoException("Partida não encontrada."));
+    }
+
+    public Page<Partidas> getPartidas(Pageable pageable) {
+        return partidasRepository.findAll(pageable);
+    }
+
+    //deletar partidas
+    public void deletePartida(Long id) {
+        if (!partidasRepository.existsById(id)) {
+            throw new DadoNaoEncontradoException("Partida não encontrado");        }
+        partidasRepository.deleteById(id);
     }
 
 }
